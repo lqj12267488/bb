500e0b38893242522e5bd01ffebde11578a29df2
lqj <lqj_0704@aliyun.com>
2019-04-17 12:33:45 +0800

    修改1

--- ssm_dao/src/main/java/cn/itcast/dao/LogDao.java
+++ ssm_dao/src/main/java/cn/itcast/dao/LogDao.java
@@ -9,5 +9,4 @@ public interface LogDao {
     @Insert("insert into sys_log values(com_sequence.nextval,#{visitTime},#{username},#{ip},#{method},#{executeTime},#{executeResult},#{executeMsg})")
     void add(SysLog log);
     //注释
-    //111
 }
